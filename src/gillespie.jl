d1c61462bfa09ddcb68f09cddcbbc7bc4f43a025
gregorykimmel <gregory.kimmel89@gmail.com>
2021-07-22 21:41:19 -0400

    Abstracted much of the code. Added montecarlo method

--- src/gillespie.jl
+++ src/gillespie.jl
@@ -1,57 +1,66 @@
-using Parameters
+module Gillespie
 
-mutable struct solution{T<:Number}
+using Parameters, Plots
 
-    current_population::Vector{Int}
+export Solution, GillespieParameters,simulate
+
+mutable struct Solution{S,T<:Number,U}
+
+    current_population::S
     current_time::T
-    population_array::Vector{Array{Int}}
+    population_array::U  #Vector{Array{Int}}
     time_array::Vector{T}
 
 end
 
-struct gillespie_parameters{T<:Number}
-    max_events::Int                 # Maximum number of events that can occur
-    final_time::Number              # Final time of simulation
-    store_array::Bool               # Check whether we fill the pop/time Array
-    initial_population::Vector{Int}
-    initial_time::T
-    sto_mat::Vector{Vector{Int}}    # How does population change?
-    compute_rates::Function         # output should be same length as sto_mat
-    params::Vector{Number}          # To be fed into compute_rates function
+Solution(pop,time) = Solution(pop,time,[pop],[time])
+
+abstract type AbstractReactionNetwork end
+
+struct ReactionMatrix{P} <: AbstractReactionNetwork
+    sto_mat::Matrix{Int}        # How does population change?
+    compute_rates::Function     # output should be same length as sto_mat
+    rate_params::P              # To be fed into compute_rates function
 end
 
-function plotfig(sol::solution,stride::Int=1)
+struct ReactionNetwork{P} <: AbstractReactionNetwork
+    sto_mat::Vector{Vector{Int}}    # How does population change?
+    compute_rates::Function         # output should be same length as sto_mat
+    rate_params::P                  # To be fed into compute_rates function
+end
+
+struct GillespieProblem{S<:Integer,T<:Number,R<:AbstractReactionNetwork}
+    max_events::S                   # Maximum number of events that can occur
+    initial_population::Vector{Int}
+    time_domain::Tuple{T,T}
+    rn::R
+end
+
+struct MonteCarloProblem
+    n::Int
+    p::GillespieProblem
+end
+
+
+function Plots.plot(sol::Solution,stride::Int=1)
 
     plot(
         sol.time_array[1:stride:end],
-        vcat(sol.population_array...)[1:stride:end],
+        hcat(sol.population_array[1:stride:end]...)',
         linecolor=:black,label=nothing
         )
 
 end
 
-function plotfig!(sol::solution,stride::Int=1)
+function Plots.plot!(sol::Solution,stride::Int=1)
 
     plot!(
         sol.time_array[1:stride:end],
-        vcat(sol.population_array...)[1:stride:end],
+        hcat(sol.population_array[1:stride:end]...)',
         linecolor=:black,label=nothing
         )
 
 end
-
-function compute_rates(current_time,current_population,params::Vector{Number})
-
-    λ0,μ0,K0, σ = params;
-
-    λ = λ0
-    μ = μ0
-    K = K0*(1.0 + σ*sin(current_time) )
-
-    return λ*current_population[1]*(1.0 - current_population[1]/K), μ*current_population[1]
-
-end
-
 
 function find_event(rates)
 
@@ -59,7 +68,7 @@ function find_event(rates)
     rate_total = sum(rates)
 
     # Compute tau (time till next event)
-    tau = -log(rand())/rate_total
+    τ = -log(rand())/rate_total
 
     # Normalize rates to determine which event occurs
     cum_rate = cumsum(rates./rate_total)
@@ -74,19 +83,25 @@ function find_event(rates)
     # find out which individual process occurred
     which_event = findfirst(x-> x >= roll,cum_rate)
 
-    return tau, which_event
+    return τ, which_event
 
 end
 
-function run_gillespie(parameters::gillespie_parameters)
+t₀(p::GillespieProblem) = p.time_domain[begin]
+t₁(p::GillespieProblem) = p.time_domain[end]
 
-    # unpack parameters into variables
-    @unpack (max_events,final_time,store_array,initial_population,
-     initial_time,sto_mat) = parameters
+function simulate(p::GillespieProblem)
+
+    #=
+    max_events::S                   # Maximum number of events that can occur
+        initial_population::Vector{Int}
+        time_domain::Tuple{T,T}
+        reaction_network::R
+    =#
 
     # Initialize the elements that will fill the solution struct
-    current_population = initial_population
-    current_time = initial_time
+    current_population = p.initial_population
+    current_time = t₀(p)
 
     # Initialize the solution array
     pop_array, time_array = Vector{Array{Int}}(),Vector{Float64}()
@@ -95,32 +110,39 @@ function run_gillespie(parameters::gillespie_parameters)
     push!(pop_array,current_population)
 
     # Begin loop to run over Gillespie until max_events or final time is reached
-    for event = 1 : max_events
+    for event = 1 : p.max_events
+
+        # println(current_population)
 
         # Compute population rates
-        rates = compute_rates(current_time,current_population)#,rate_params)
+        rates = p.rn.compute_rates(current_time,current_population,p.rn.rate_params)
 
         # Zero propagation
         if sum(rates) == 0
             break
         end
 
-        tau, which_event = find_event(rates)
+        τ, which_event = find_event(rates)
+
+        if τ < 0
+            @warn "something wrong"
+            @show rates
+        end
 
         # update time and population
-        current_time += tau
-        current_population += sto_mat[which_event]
+        current_time += τ
+        current_population += p.rn.sto_mat[which_event]
 
         push!(time_array,current_time)
         push!(pop_array,current_population)
 
-        if current_time >= final_time
+        if current_time >= t₁(p)
             break
         end
 
     end
 
-    return solution(
+    return Solution(
         current_population,
         current_time,
         pop_array,
@@ -130,5 +152,8 @@ function run_gillespie(parameters::gillespie_parameters)
 
 end
 
+simulate(mcp::MonteCarloProblem) = map(1:mcp.n) do i
+    run_gillespie(mcp.p)
+end
 
-
+end
